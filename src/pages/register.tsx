a8783541e3810aca1f4c22b5319228a1272ec6a1
Elvio Petillo <elvio.petillo@nanowaris.com>
2022-04-03 18:19:15 +0200

    Partially implement register form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,24 +1,109 @@
 import * as React from 'react';
-import { Box, Paper, Typography } from '@mui/material';
+import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { gql, useMutation } from '@apollo/client';
+import { User } from '../models/User';
 
 const Register = () => {
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const REGISTER_MUTATION = gql`
+      mutation {
+        addUser(input: {
+          username: $username,
+          email: $email,
+          password: $password
+          }) {
+		      id
+		      username
+          verified
+	      }
+      }
+    `;
+    
+    
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [addUser, { data, loading, error }] = useMutation<User>(REGISTER_MUTATION);
+    addUser({variables: { username: formData.get('username') }})
+  };
+   
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: 'whitesmoke',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Paper
-        elevation={3}
-        sx={{ padding: '1rem', backgroundColor: 'secondary.dark' }}
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
       >
-        <Typography variant='h3' color='white'>Register</Typography>
-      </Paper>
-    </Box>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+      Sign up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                type="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            {/*           <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox value="allowExtraEmails" color="primary" />}
+              label="I want to receive inspiration, marketing promotions and updates via email."
+            />
+          </Grid> */}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+        Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+            Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box> 
+    </Container>
   );
 };
 
