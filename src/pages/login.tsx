91406fa64a09ff651141ac15b8b8375c26416a2f
jemai <jerome.maier@uzh.ch>
2022-04-04 22:38:52 +0200

    login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,113 @@
 import * as React from 'react';
-import { Box, Paper, Typography } from '@mui/material';
-import { gql, useSubscription } from '@apollo/client';
+import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { gql, useMutation } from '@apollo/client';
+import { User } from '../models/User';
+//import { gql, useSubscription } from '@apollo/client';
 
 const Login = () => {
-  const GREETINGS_SUBSCRIPTION = gql`
+
+  const LOGIN_USER = gql`
+      mutation signIn($user: LoginInput!) {
+        login(input: $user) {
+		      id
+		      username
+          email
+          #verified
+	      }
+      }
+    `;
+
+  interface Login {
+    username: FormDataEntryValue | null;
+    password: FormDataEntryValue | null;
+  }
+
+  interface LoginData {
+    user: Login;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loginUser, { data, loading, error }] = useMutation<User, LoginData>(LOGIN_USER);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    loginUser({
+      variables: {
+        user: {
+          username: formData.get('username'),
+          password: formData.get('password') }
+      }
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="username"
+            label="Username"
+            id="username"
+            autoComplete="username"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          {/*<FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />*/}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {'Don\'t have an account? Sign Up'}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+
+  /*const GREETINGS_SUBSCRIPTION = gql`
   subscription {
     greetings
   }
@@ -39,7 +143,7 @@ const Login = () => {
         <Typography variant='caption'>{LatestGreetings()}</Typography>
       </Paper>
     </Box>
-  );
+  );*/
 };
 
 export default Login;
