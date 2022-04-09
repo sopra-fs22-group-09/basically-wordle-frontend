7385a44774dcebe64c50dc415823cdb0abc080cb
Elvio Petillo <elvio.petillo@nanowaris.com>
2022-04-09 14:42:58 +0200

    Add linting stuff

---
+++ .eslintrc.prod.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  extends: [
+    './.eslintrc.js'
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': 'error',
+  }
+};
