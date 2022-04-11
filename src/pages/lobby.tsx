abed0f2617632884f59a6899f04f41f0221a728b
Mete Polat <81754505+polatmete@users.noreply.github.com>
2022-04-11 17:32:05 +0200

    Beautify lobby, still in work

--- src/pages/lobby.tsx
+++ src/pages/lobby.tsx
@@ -40,130 +40,119 @@ const Lobby = () => {
   return (
     <Box
       sx={{
-        position:'absolute',
-        height:'90%',
         width:'90%',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)'
+        mx:'auto',
+        mt:'2.5%',
+        textAlign: 'center'
       }}
     >
-      <Stack sx={{ display:'flex', height:'100%' }}>
-        <Box sx={{ m:'auto', mb:4, border:'solid 2px white' }}>
-          <Typography variant='h4'>
-            {'jemaie\'s Game'}
+      <Typography variant='h1' sx={{fontSize: 48}}> {/*TODO Naja...*/}
+        {'jemaie\'s Game'}
+      </Typography>
+      <Box sx={{ width: '66%!important', float: 'left', border:'solid 2px orange' }}>
+        <Box sx={{ width: '49%', border:'solid 2px red', float: 'left' }}>
+          <Typography variant='h5'>
+            players
           </Typography>
+          <List>
+            <ListItem>
+              player1
+            </ListItem>
+            <ListItem>
+              player2
+            </ListItem>
+          </List>
         </Box>
-        <Stack spacing={5} direction="row" sx={{ m:'auto', border:'solid 2px green' }}>
-          <Stack spacing={2} sx={{ m:'auto', border:'solid 2px yellow' }}>
-            <Stack spacing={2} direction="row" sx={{ m:'auto', border:'solid 2px blue' }}>
-              <Stack sx={{ width:150, m:'auto', border:'solid 2px red' }}>
-                <Box sx={{ m:'auto', mb:2, border:'solid 2px white' }}>
-                  <Typography variant='h5'>
-                    players
-                  </Typography>
-                </Box>
-                <List>
-                  <ListItem>
-                    player1
-                  </ListItem>
-                  <ListItem>
-                    player2
-                  </ListItem>
-                </List>
-              </Stack>
-              <Stack sx={{ width:200, m:'auto', border:'solid 2px red' }}>
-                <Box sx={{ m:'auto', mb:2, border:'solid 2px white' }}>
-                  <Typography variant='h5'>
-                    settings
-                  </Typography>
-                </Box>
-                <FormControl sx={{ width:150, m:2 }}>
-                  <InputLabel>Game Mode</InputLabel>
-                  <Select
-                    value={gameMode}
-                    label="Game Mode"
-                    onChange={(event) => setGameMode(event.target.value as string)}
-                  >
-                    {gameModes?.map(mode => {
-                      return (
-                        <MenuItem key={mode.label} value={mode.label}>
-                          {mode.label}
-                        </MenuItem>
-                      );
-                    })}
-                  </Select>
-                </FormControl>
-                Lobby Size
-                <Slider
-                  sx={{ m:'auto', width:'80%' }}
-                  marks
-                  step={1}
-                  min={1}
-                  max={6}
-                  valueLabelDisplay='auto'
-                  value={lobbySize}
-                  onChange={(event, newSize) => setLobbySize(newSize as number)}
+        <Box sx={{ width: '49%', border:'solid 2px red', float: 'right' }}>
+          <Typography variant='h5'>
+            settings
+          </Typography>
+          <FormControl sx={{ width:150, m:2 }}>
+            <InputLabel>Game Mode</InputLabel>
+            <Select
+              value={gameMode}
+              label="Game Mode"
+              onChange={(event) => setGameMode(event.target.value as string)}
+            >
+              {gameModes?.map(mode => {
+                return (
+                  <MenuItem key={mode.label} value={mode.label}>
+                    {mode.label}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <Typography variant='h6'>Lobby Size</Typography>
+          <Slider
+            sx={{ m:'auto', width:'80%' }}
+            marks
+            step={1}
+            min={1}
+            max={6}
+            valueLabelDisplay='auto'
+            value={lobbySize}
+            onChange={(event, newSize) => setLobbySize(newSize as number)}
+          />
+          <Typography variant='h6'>Rounds</Typography>
+          <Slider
+            sx={{ m:'auto', width:'80%' }}
+            marks
+            step={1}
+            min={1}
+            max={3}
+            valueLabelDisplay='auto'
+            value={gameRounds}
+            onChange={(event, newRounds) => setGameRounds(newRounds as number)}
+          />
+          <Autocomplete //TODO: wie chume a di usgwählte?
+            sx={{ m:'auto', width:'80%' }}
+            multiple
+            readOnly={!localStorage.getItem('token')}
+            disableCloseOnSelect
+            options={wordCategories}
+            getOptionLabel={(option) => option.category}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+                  checkedIcon={<CheckBoxIcon fontSize="small" />}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
                 />
-                Rounds
-                <Slider
-                  sx={{ m:'auto', width:'80%' }}
-                  marks
-                  step={1}
-                  min={1}
-                  max={3}
-                  valueLabelDisplay='auto'
-                  value={gameRounds}
-                  onChange={(event, newRounds) => setGameRounds(newRounds as number)}
-                />
-                <Autocomplete //TODO: wie chume a di usgwählte?
-                  sx={{ m:'auto', width:'80%' }}
-                  multiple
-                  readOnly={!localStorage.getItem('token')}
-                  disableCloseOnSelect
-                  options={wordCategories}
-                  getOptionLabel={(option) => option.category}
-                  renderOption={(props, option, { selected }) => (
-                    <li {...props}>
-                      <Checkbox
-                        icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
-                        checkedIcon={<CheckBoxIcon fontSize="small" />}
-                        style={{ marginRight: 8 }}
-                        checked={selected}
-                      />
-                      {option.category}
-                    </li>
-                  )}
-                  renderInput={(params) => (
-                    <TextField {...params} placeholder="Word Categories" />
-                  )}
-                />
-              </Stack>
-            </Stack>
-            <Box>
-              <TextField
-                type='text'
-                defaultValue='this is a link to copy'
-                variant='outlined'
-                InputProps={{ readOnly: true,
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Button>
-                        COPY
-                      </Button>
-                    </InputAdornment>)}}
-              />
-            </Box>
-            <Box>
-              <Button variant="contained" sx={{ mr:2 }}>Leave Lobby</Button>
-              <Button variant="contained" sx={{ ml:2 }}>Start Game</Button>
-            </Box>
-          </Stack>
-          <Box sx={{ width:150, m:'auto', border:'solid 2px white' }}>
-            chat
-          </Box>
-        </Stack>
-      </Stack>
+                {option.category}
+              </li>
+            )}
+            renderInput={(params) => (
+              <TextField {...params} placeholder="Word Categories" />
+            )}
+          />
+        </Box>
+        <br />
+        <Box sx={{clear: 'both', width: '66%', maxWidth: 500, mt: 38, mx: 'auto', textAlign: 'left', border:'solid 2px blue'}}>
+          <TextField
+            type='text'
+            defaultValue='this is a link to copy'
+            variant='outlined'
+            InputProps={{ readOnly: true,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Button>
+                    COPY
+                  </Button>
+                </InputAdornment>)}}
+            sx={{
+              width: '100%'
+            }}
+          />
+          <br />
+          <Button variant="contained" sx={{ mt:2 }}>Leave Lobby</Button>
+          <Button variant="contained" sx={{ mt:2, float: 'right'}}>Start Game</Button>
+        </Box>
+      </Box>
+      <Box sx={{ float: 'right', width: '33%!important', height: 'calc(100vh - 300px)', border:'solid 2px white' }}>
+        chat
+      </Box>
     </Box>
   );
 };
