4470de82a250be87a4e5528a8165fe7f5ed001ca
Elvio <elvio.petillo@nanowaris.com>
2022-04-19 20:25:34 +0200

    Fix cache warning

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+export type Maybe<T> = T | null;
+
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // Date: any;
+};
