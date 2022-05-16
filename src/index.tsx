13158deace10278a2130b3c8695974d5bbbcba65
Elvio Petillo <elvio.petillo@nanowaris.com>
2022-05-17 01:41:07 +0200

    React 18 activated

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import { ApolloProvider } from '@apollo/client';
 //import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 import React from 'react';
-//import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+//import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 import api from './services/api';
 
 // eslint-disable-next-line import/no-named-as-default-member
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ApolloProvider client={api}>
@@ -22,10 +22,10 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('app')
-);
+);*/
 
 // FIXME: The React 18 way. As you might have guessed, it doesn't work: https://github.com/apollographql/apollo-client/issues/9664
-/*const container = document.getElementById('app');
+const container = document.getElementById('app');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(
@@ -38,7 +38,7 @@ root.render(
       </ApolloProvider>
     </Provider>
   </React.StrictMode>
-);*/
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
