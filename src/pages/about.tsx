706d79b3f385ecfdc5e928e099d0ce836b124050
Mete Polat <81754505+polatmete@users.noreply.github.com>
2022-05-25 14:28:36 +0200

    About page (#182)
    * Added info to About page
    
    * Make About page responsive again, remove unnessesary css, code cleanup and closes #150, closes #131
    
    Co-authored-by: bzns <matej.gurica@uzh.ch>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,27 +1,36 @@
 import * as React from 'react';
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Link, List, ListItem, Typography, useMediaQuery, useTheme } from '@mui/material';
 
 const About = () => {
   const theme = useTheme();
   const smallScreen = !useMediaQuery(theme.breakpoints.up('mobile')); //screen smaller than defined size
 
-  // TODO This is just a template. Feel free to edit anything but the topmost Box.
-  // TODO Please don't just add some text, make it look nice and fancy.
   return (
-    <Box sx={{width: '90%', mx:'auto', mt: '20px', textAlign: smallScreen ? 'justify' : 'center'}}>
+    <Box sx={{width: '90%', maxWidth: '1200px', mx:'auto', mt: '20px', textAlign: smallScreen ? 'justify' : 'center', fontSize: '17px'}}>
       <Typography variant="h1" sx={{fontSize: '48px', textAlign: 'center'}}>About Basically Wordle.</Typography>
-      <Typography variant="h2" sx={{fontSize: '42px', mt: '20px', textAlign: 'center'}}>What is Wordle?</Typography>
-      <Typography variant="body1" sx={{fontSize: '21px'}}>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      <Typography variant="h2" sx={{fontSize: '30px', mt: '48px', textAlign: 'center'}}>What is Wordle?</Typography>
+      <Typography variant="body1">
+        <i>Wordle</i> is a fun online word game originally developed by Josh Wardle and subsequently acquired and published by the New York Times. The goal of the game is to guess a five-letter word by submitting a maximum of six guesses, with the letters of each guess taking on a green color for every letter in the right spot, or a yellow color if the letter is contained in the word but in a different position. <i>Basically Wordle</i> is a more sophisticted adaptation of this concept, incorporating features like various PvP game modes and a friend system.
       </Typography>
-      <Typography variant="h2" sx={{fontSize: '42px', mt: '20px', textAlign: 'center'}}>About us</Typography>
-      <Typography variant="body1" sx={{fontSize: '21px'}}>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      <Typography variant="h2" sx={{fontSize: '30px', mt: '40px', textAlign: 'center'}}>About us</Typography>
+      <Typography variant="body1">
+        We (Elvio Petillo, Jerome Maier, Mete Polat, Matej Gurica) are four computer science students at the University of Zurich, where we developed this application as our project for the course <i>Software Engineering Lab</i> held by Prof. Dr. Thomas Fritz during the spring semester of 2022. Our group was advised and supervised by teaching assistant Matthias Mylaeus throughout the entire project.
       </Typography>
-      <Typography variant="h2" sx={{fontSize: '42px', mt: '20px', textAlign: 'center'}}>Why did we develop this app?</Typography>
-      <Typography variant="body1" sx={{fontSize: '21px'}}>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      <Typography variant="h2" sx={{fontSize: '30px', mt: '40px', textAlign: 'center'}}>Why did we develop this app?</Typography>
+      <Typography variant="body1">
+        The main motivation for developing this app was to learn and get a good understanding of the technologies most commonly used when creating responsive client-server applications. In order to achieve this goal, we considered many different program ideas, but finally settled on the plan of making a more complex and interactive version of the popular <i>Wordle</i> game, which finally turned into <i>Basically Wordle.</i>
       </Typography>
+      <Typography variant="h2" sx={{fontSize: '30px', mt: '40px', textAlign: 'center'}}>Technologies used</Typography>
+      <Typography variant="body1">The application is built using the following technologies and frameworks:</Typography>
+      <Box sx={{mx: 'auto', width: '90%', maxWidth: '315px'}}>
+        <List sx={{listStyle: 'disc'}}>
+          <ListItem sx={{display: 'list-item'}}>Apollo/GraphQL -&nbsp;<Link href={'https://www.apollographql.com'} title="Apollo/GraphQL" target="_blank" rel="nofollow noopener noreferrer">apollographql.com</Link></ListItem>
+          <ListItem sx={{display: 'list-item'}}>Reactor Core -&nbsp;<Link href={'https://projectreactor.io'} title="Reactor Core" target="_blank" rel="nofollow noopener noreferrer">projectreactor.io</Link></ListItem>
+          <ListItem sx={{display: 'list-item'}}>Dokku -&nbsp;<Link href={'https://dokku.com'} title="Dokku" target="_blank" rel="nofollow noopener noreferrer">dokku.com</Link></ListItem>
+          <ListItem sx={{display: 'list-item'}}>ReactJS -&nbsp;<Link href={'https://reactjs.org'} title="ReactJS" target="_blank" rel="nofollow noopener noreferrer">reactjs.org</Link></ListItem>
+          <ListItem sx={{display: 'list-item'}}>Spring Boot -&nbsp;<Link href={'https://spring.io/projects/spring-boot'} title="Spring Boot" target="_blank" rel="nofollow noopener noreferrer">spring.io</Link></ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
