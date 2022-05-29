1ec0246a297494f9ccd8481b379d3bb325388597
Mete Polat <81754505+polatmete@users.noreply.github.com>
2022-05-29 18:08:07 +0200

    Change guesses in tutorial, simplify tutorial

--- src/pages/tutorial.tsx
+++ src/pages/tutorial.tsx
@@ -14,29 +14,22 @@ const Tutorial = () => {
       }
   `;
 
-  interface TutorialType {
-    tutorialFinished: boolean;
-  }
+  interface TutorialType {tutorialFinished: boolean;}
 
   const Grid = lazy(() => import('../components/grid/grid'));
   const Keyboard = lazy(() => import('../components/keyboard/keyboard'));
-
   const navigate = useNavigate();
   const theme = useTheme();
-
   const [finishTutorial, { loading, error }] = useMutation<TutorialType>(FINISH_TUTORIAL);
   const [count, setCount] = useState(0);
   const smallScreen = !useMediaQuery(theme.breakpoints.up('mobile')); //screen smaller than defined size
-  const allGuesses = ['HOMIE', 'UNDER', 'ROMAN', 'ROYAL'];
+  const allGuesses = ['COMET', 'UNDER', 'ROMAN', 'ROYAL'];
   const allLetterStates = [
     [LetterState.WRONG, LetterState.CORRECTPOSITION, LetterState.WRONG, LetterState.WRONG, LetterState.WRONG],
     [LetterState.WRONG, LetterState.WRONG, LetterState.WRONG, LetterState.WRONG, LetterState.INWORD],
     [LetterState.CORRECTPOSITION, LetterState.CORRECTPOSITION, LetterState.WRONG, LetterState.CORRECTPOSITION, LetterState.WRONG],
     [LetterState.CORRECTPOSITION, LetterState.CORRECTPOSITION, LetterState.CORRECTPOSITION, LetterState.CORRECTPOSITION, LetterState.CORRECTPOSITION],
   ];
-  const letterOnCorrectPosition = ['', 'O', 'O', 'ORA', 'ORAYL'];
-  const letterInWord = ['', '', 'R', '', ''];
-  const letterNotInWord = ['', 'HMIE', 'HMIEUND', 'HMIEUNDN', 'HMIEUNDN'];
   const text = [
     'Wordle is a fun game where the player tries to guess a word consisting of five letters in as few tries as possible. The first step is to enter any five-letter word.',
     'If the entered word contains a letter from the target word and this letter is also in the correct position, it will turn green. Blacked out letters do not appear in the target word.',
@@ -44,35 +37,41 @@ const Tutorial = () => {
     'We are getting closer...',
     'Ta-da! We have found the target word! If you do not manage to solve the riddle in a maximum of six tries, you lose the game. Now go and have some fun!'
   ];
-  const maxCount = Math.min(letterOnCorrectPosition.length, letterInWord.length, letterNotInWord.length) - 1; // Since only those can cause OutOfBound
 
   const getAllGuesses = () => {
     const tmp = [allGuesses[0]];
-    let i: number;
-    for (i = 1; i < count; ++i) tmp.push(allGuesses[i]);
+    for (let i = 1; i < count; ++i) tmp.push(allGuesses[i]);
     while (tmp.length < 6) tmp.push('');
     return tmp;
   };
 
   const getAllLetterStates = () => {
-    if (count === 0) return [[]];
-    const tmp = [];
-    let i: number;
-    for (i = 0; i < count; ++i) tmp.push(allLetterStates[i]);
+    const tmp: LetterState[][] = [];
+    if (count < 1) return tmp;
+    for (let i = 0; i < count; ++i) tmp.push(allLetterStates[i]);
     while (tmp.length < 6) tmp.push([]);
     return tmp;
   };
 
+  const getLetterStates = (operation: LetterState) => {
+    let tmp = '';
+    if (count < 1) return tmp;
+    for (let i = operation === LetterState.INWORD ? count - 1 : 0; i < count; ++i)
+      for (let j = 0; j < allLetterStates[i].length; ++j)
+        tmp += allLetterStates[i][j] == operation ? !tmp.includes(allGuesses[i].split('')[j]) ? allGuesses[i].split('')[j] : '' : '';
+    return tmp;
+  };
+
   const handleSubmit = () => {
-    if (count === maxCount - 1) finishTutorial();
-    else if (count === maxCount) navigate('/');
+    if (count === allGuesses.length - 1) finishTutorial();
+    else if (count === allGuesses.length) navigate('/');
     setCount(count + 1);
   };
   
   return (
     <Box sx={{width: smallScreen ? '100%' : '90%', maxWidth: '700px', mx: 'auto', textAlign: 'center'}}>
       <Typography variant="h1" fontSize="48px" sx={{mt: '20px'}}>Tutorial</Typography>
-      {(!loading && error) && <Alert sx={{ mt: 3, minWidth: 1, maxWidth: 1 }} variant="filled" severity="error">{error.message}</Alert>}
+      {(!loading && error && count === allGuesses.length) && <Alert sx={{my: '20px', mx: 'auto', maxWidth: smallScreen ? '90%' : '100%'}} variant="filled" severity="error">{error.message}</Alert>}
       <Suspense fallback={<LoaderCenterer><Orbit size={35} color={theme.additional.UiBallLoader.colors.main} /></LoaderCenterer>}>
         <Grid
           allGuesses={getAllGuesses()}
@@ -82,9 +81,9 @@ const Tutorial = () => {
       </Suspense>
       <Suspense fallback={<LoaderCenterer><Orbit size={35} color={theme.additional.UiBallLoader.colors.main}/></LoaderCenterer>}>
         <Keyboard
-          letterOnCorrectPosition={letterOnCorrectPosition[count]}
-          letterInWord={letterInWord[count]}
-          letterNotInWord={letterNotInWord[count]}
+          letterOnCorrectPosition={getLetterStates(LetterState.CORRECTPOSITION)}
+          letterInWord={getLetterStates(LetterState.INWORD)}
+          letterNotInWord={getLetterStates(LetterState.WRONG)}
         />
       </Suspense>
       <Typography sx={{minHeight: smallScreen ? '96px' : '48px', width: smallScreen ? '90%' : '100%', mx: 'auto', mt: '20px', textAlign: 'center'}} variant="body1">{text[count]}</Typography>
@@ -108,14 +107,14 @@ const Tutorial = () => {
           }}
           variant="body1"
         >
-          {count + 1} / {maxCount + 1}
+          {count + 1} / {allGuesses.length + 1}
         </Typography>
         <Button
           onClick={handleSubmit}
           variant="contained"
           sx={{width: smallScreen ? '90%' : '33%', height: '100%', float: smallScreen ? 'none' : 'right'}}
         >
-          {count < maxCount ? 'Next' : 'Let\'s play!'}
+          {count < allGuesses.length ? 'Next' : 'Let\'s play!'}
         </Button>
       </Box>
     </Box>
